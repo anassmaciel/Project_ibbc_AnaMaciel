e0958eeba4c51a70b0da75e50614cb420d94fd18
anassmaciel <ana_sofiasm@hotmail.com>
2025-12-03 06:55:22 +0000

    Update script3_av.sh

--- script3_av.sh
+++ script3_av.sh
@@ -7,8 +7,6 @@ MAIN_DIR="$(pwd)/Project_ibbc_$MY_NAME"
 
 # Create the archive inside MAIN_DIR
 echo "[INFO] Compressing results..."
-tar -czf "$MAIN_DIR/${MY_NAME}_results.tar.gz" \
-    "$MAIN_DIR/results" \
-    "$MAIN_DIR/logs"
+zip -r "${MAIN_DIR}/${MY_NAME}_results.zip" "$MAIN_DIR/results" "$MAIN_DIR/logs"
 echo "[INFO] Done!"
 
